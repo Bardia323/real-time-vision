e90b80392a1a2fed5bc68a63420243a3926c41e5
Bardia Shahrestani <Bardia.shahrestani@gmail.com>
2023-07-19 02:51:08 -0400

    Update install-windows.cmd

--- install-windows.cmd
+++ install-windows.cmd
@@ -9,7 +9,7 @@ python -m venv venv
 REM activate the virtual environment
 call venv\Scripts\activate
 
-REM install pytorch
+REM install pytorch (please replace this command for the proper torch version for your system)
 pip install --pre torch torchvision torchaudio -f https://download.pytorch.org/whl/nightly/cu121/torch_nightly.html
 
 REM install necessary packages
@@ -18,6 +18,6 @@ python -m pip install -r requirements.txt
 REM setup config
 python diffusers-server\config.py
 
-REM call vbs script to create shortcuts
-cscript //nologo create_shortcuts.vbs
+REM call vbs script to create shortcuts (probably useless)
+REM cscript //nologo create_shortcuts.vbs
 
